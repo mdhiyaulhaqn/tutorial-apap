1052caf076aacab5982dd21dc7602c61ed68c1e7
Muhammad Dhiyaulhaq Nugraha <dhiyaulhaqnugraha@gmail.com>
2019-10-31 20:21:35 +0700

    feat : add find Recipe Rest 'Feature

--- gopud/src/main/java/apap/tutorial/gopud/rest/Setting.java
+++ gopud/src/main/java/apap/tutorial/gopud/rest/Setting.java
@@ -2,4 +2,5 @@ package apap.tutorial.gopud.rest;
 
 public class Setting {
     final public static String restoranurl = "https://0e1733ad-5aba-40bb-b3c2-32d853d34b4b.mock.pstmn.io";
+    final public static String recipesurl = "https://api.spoonacular.com";
 }
