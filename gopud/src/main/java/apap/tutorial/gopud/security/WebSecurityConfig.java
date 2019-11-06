b06add28ab8b9a71be81f4bd2f1b863be2728d91
Muhammad Dhiyaulhaq Nugraha <dhiyaulhaqnugraha@gmail.com>
2019-11-06 11:35:38 +0700

    feat : mengambil username dari database

--- gopud/src/main/java/apap/tutorial/gopud/security/WebSecurityConfig.java
+++ gopud/src/main/java/apap/tutorial/gopud/security/WebSecurityConfig.java
@@ -1,12 +1,14 @@
 package apap.tutorial.gopud.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
@@ -20,6 +22,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/css/**").permitAll()
                 .antMatchers("/js/**").permitAll()
+                .antMatchers("/restoran/**").hasAnyAuthority("MERCHANT")
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
@@ -33,11 +36,20 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
+//    @Autowired
+//    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
+//        auth.inMemoryAuthentication()
+//                .passwordEncoder(encoder())
+//                .withUser("nadiem").password(encoder().encode("makarim"))
+//                .roles("USER");
+//    }
+
+    @Qualifier("userDetailsServiceImpl")
     @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
-        auth.inMemoryAuthentication()
-                .passwordEncoder(encoder())
-                .withUser("nadiem").password(encoder().encode("makarim"))
-                .roles("USER");
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception{
+        auth.userDetailsService(userDetailsService).passwordEncoder(encoder());
     }
 }
