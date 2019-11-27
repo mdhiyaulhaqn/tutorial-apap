ef39dc92bb3c753ab70afc5654dc8f0fdc2e554d
Muhammad Dhiyaulhaq Nugraha <dhiyaulhaqnugraha@gmail.com>
2019-11-27 14:05:47 +0700

    feat: view all restoran static

--- frontend-tutorial9/src/containers/Restorans/Restorans.js
+++ frontend-tutorial9/src/containers/Restorans/Restorans.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import Restoran from '../../components/Restoran/Restoran';
+import classes from './Restorans.module.css'
 
 class Restorans extends Component{
     constructor(props){
@@ -10,32 +12,42 @@ class Restorans extends Component{
                 {id: 2, nama: "Restoran B", alamat: "This is address Restoran B", nomorTelepon: "021120491"},
                 {id: 3, nama: "Restoran C", alamat: "This is address Restoran C", nomorTelepon: "021940256"},
             ],
-            isLoading: false
+            isLoading: true
         }
     }
 
-    componentDidMount(){
-        console.log("componentDidMount()");
-    }
+    // componentDidMount(){
+    //     console.log("componentDidMount()");
+    // }
 
-    shouldComponentUpdate(nextProps, nextState){
-        console.log("shouldComponentUpdate()");
-        return true;
-    }
+    // shouldComponentUpdate(nextProps, nextState){
+    //     console.log("shouldComponentUpdate()");
+    //     return true;
+    // }
 
-    loadingHandler = () => {
-        const currentIsLoading = this.state.isLoading;
-        this.setState( {isLoading: !(currentIsLoading)} );
-        console.log(this.state.isLoading);
-    }
+    // loadingHandler = () => {
+    //     const currentIsLoading = this.state.isLoading;
+    //     this.setState( {isLoading: !(currentIsLoading)} );
+    //     console.log(this.state.isLoading);
+    // }
 
     render(){
-        console.log("render()");
+        // console.log("render()");
         return(
             <React.Fragment>
-                <div> All Restorans </div>
-                <div> Restorans 1, 2, 3, etc </div>
-                <button onClick={this.loadingHandler}>changeState</button>
+                <div className={classes.Title}> All Restorans </div>
+                <div className={classes.Restorans}>
+                    {this.state.restorans.map(restoran =>
+                        <Restoran
+                            key={restoran.id}
+                            nama={restoran.nama}
+                            alamat={restoran.alamat}
+                            nomorTelepon={restoran.NomorTelepeon}
+                            />)
+                        }
+                </div>
+
+                {/* <button onClick={this.loadingHandler}>changeState</button> */}
             </React.Fragment>
         );
     }
