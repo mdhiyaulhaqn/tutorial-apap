0e797f684f53320fed5839cf6e7d1f4faf8f3cde
Muhammad Dhiyaulhaq Nugraha <dhiyaulhaqnugraha@gmail.com>
2019-12-03 18:10:45 +0700

    feat: demo tutorial 8

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import List from "./components/List";
 import dummyItems from "./items.json";
+import classes from './App.module.css';
 
 export default class App extends React.Component{
   constructor(props){
@@ -8,9 +9,11 @@ export default class App extends React.Component{
     this.state={
       checked: false,
       favItems: [],
-
+      isDarkMode: false,
     };
     this.handleShowFavorite =  this.handleShowFavorite.bind(this);
+
+    this.handleDarkMode = this.handleDarkMode.bind(this);
   }
 
   handleItemClick = item => {
@@ -31,35 +34,44 @@ export default class App extends React.Component{
         })
     }
 
+    handleDarkMode(){
+      this.setState({
+        isDarkMode: !this.state.isDarkMode
+      })
+    }
+
     render(){
       const { favItems } = this.state;
 
       return(
           <div className="container-fluid">
-            <h1 className="text-center">
-                Welcome !
-              <small>Class-based</small>
-            </h1>
-            <div style={{textAlign:"right"}}>
-                <label><input type="checkbox" onChange={this.handleShowFavorite}/> Show Favorit</label>
-            </div>
-            <div className="container pt-3">
-              <div className="row">
-                <div className="col-sm">
-                  <List
-                    title="Our Menu"
-                    items={dummyItems}
-                    onItemClick={this.handleItemClick}
-                    hidden={true}
-                    />
-                </div>
-                <div className="col-sm" hidden={!this.state.checked}>
-                  <List
-                    title="My Favorite"
-                    items={favItems}
-                    onItemClick={this.handleItemClick}
-                    hidden={false}
-                    />
+            <div className={this.state.isDarkMode ? classes.WhiteModeBg:classes.DarkModeBg}>
+              <h1 className="text-center">
+                  <span className={ this.state.isDarkMode ?classes.WhiteModeP : classes.DarkModeP}>Welcome !</span>
+                <small>Class-based</small>
+              </h1>
+              <div style={{textAlign:"right"}}>
+                  <label><input type="checkbox" onChange={this.handleShowFavorite}/> Show Favorit  </label>
+                  <label><input type="checkbox" onChange={this.handleDarkMode}/> Dark Mode </label>
+              </div>
+              <div className="container pt-3">
+                <div className="row">
+                  <div className="col-sm">
+                    <List
+                      title="Our Menu"
+                      items={dummyItems}
+                      onItemClick={this.handleItemClick}
+                      hidden={true}
+                      />
+                  </div>
+                  <div className="col-sm" hidden={!this.state.checked}>
+                    <List
+                      title="My Favorite"
+                      items={favItems}
+                      onItemClick={this.handleItemClick}
+                      hidden={false}
+                      />
+                  </div>
                 </div>
               </div>
             </div>
